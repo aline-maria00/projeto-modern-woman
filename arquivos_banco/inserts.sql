870fc5d2c285dd1f343b00fa3ff81228da82b228
manuela14 <57487071+manuela14@users.noreply.github.com>
2019-11-07 09:35:10 -0300

    Arquivo sql - insert

---
+++ arquivos_banco/inserts.sql
@@ -0,0 +1,10 @@
+USE DATABASE `ModernWoman`;
+
+insert into Bairros (Bairro) 
+	values ("Catita");
+
+insert into Cidades (Cidade) 
+	values ("Arapiraca");
+
+insert into Estados (Estado) 
+	values ("Alagoas");
